1c2b50bd09486c309defc5bfe1320c1c4de979d6
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-05-14 18:02:06 +0200

    fix discount factors

--- R/discount_factors.R
+++ R/discount_factors.R
@@ -4,66 +4,71 @@ esgdiscountfactor <- function(r, X)
   if(missing(r) || missing(X))
     stop("'r' and 'X' must be provided")
   
-  length.r <- length(r)
-  length.X <- length(X)
-  start.r <- start(r)
+  length_r <- length(r)
+  length_X <- length(X)
+  start_r <- start(r)
   deltat.r <- deltat(r)
-  start.X <- start(X)
-  deltat.X <- deltat(X)
+  start_X <- start(X)
+  deltat_X <- deltat(X)
   
-  if(length.r == 1 && length.X == 1) 
+  if(length_r == 1 && length_X == 1) 
   {
     return(X*exp(-r)) 
   }
   
-  if(length.r == 1 && length.X != 1) 
+  if(length_r == 1 && length_X != 1) 
   {
     r <- ts(matrix(r, nrow(X), ncol(X)), 
-            start = start.X, deltat = deltat.X)
+            start = start_X, deltat = deltat_X)
     
     if(tsp(X)[1] > 0)
     {
-      return(ts(X*exp(-apply(r, 2, cumsum)*deltat.X), 
+      return(ts(X*exp(-apply(r, 2, cumsum)*deltat_X), 
                 start = 0, 
-                deltat = deltat.X))      
+                deltat = deltat_X))      
     }
     else
     {
-      Int_r <- exp(-apply(r, 2, cumsum)*deltat.X)
+      Int_r <- exp(-apply(r, 2, cumsum)*deltat_X)
       return(ts(X*rbind(rep(1, ncol(X)), 
-                        Int_r[1:(nrow(X)-1), ]),                                           , 
+                        Int_r[1:(nrow(X)-1), ]),                                            
                 start = 0, 
-                deltat = deltat.X))
+                deltat = deltat_X))
     }
   } 
   
-  if(length.r != 1 && length.X == 1)
+  if(length_r != 1 && length_X == 1)
   {
     X <- ts(matrix(X, nrow(r), ncol(r)), 
-            start = start.r, deltat = deltat.r)
+            start = start_r, deltat = deltat.r)
     
     return(ts(X*exp(-apply(r, 2, cumsum)*deltat.r),                                          
               start = 0, 
               deltat = deltat.r))
   }
   
-  if(length.r != 1 && length.X != 1)
+  if(length_r != 1 && length_X != 1)
   {
+    if (all.equal(start(X), start(r)) && all.equal(frequency(X), frequency(r)))
+    {
+      return(X*exp(-apply(r, 2, cumsum)*deltat(r)))
+    }
+    
     if(tsp(X)[1] > 0)
     {
       return(suppressWarnings(ts(X*window(exp(-apply(r, 2, cumsum)*deltat.r), 
-                                          start = start.X, 
-                                          deltat = deltat.X), 
+                                          start = start_X, 
+                                          deltat = deltat_X), 
                                  start = 0, 
-                                 deltat = deltat.X)))
+                                 deltat = deltat_X)))
     }
     else
     {
       Int_r <- ts(exp(-apply(r, 2, cumsum)*deltat.r), deltat = deltat.r)
       return(suppressWarnings(ts(X*rbind(rep(1, ncol(X)), 
-                                         Int_r[1:(nrow(X)-1), ]),                                           , 
+                                         Int_r[1:(nrow(X)-1), ]),
                                  start = 0, 
-                                 deltat = deltat.X)))
+                                 deltat = deltat_X)))
     }
   }
 }
